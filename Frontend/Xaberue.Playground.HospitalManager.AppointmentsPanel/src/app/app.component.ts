1441f820d9dfbccfbd69e1ccc9de24ea66ca3987
xaberue <xavier.aberue@outlook.es>
2025-03-21 15:49:05 +0100

    Current appointments grid done

--- Frontend/Xaberue.Playground.HospitalManager.AppointmentsPanel/src/app/app.component.ts
+++ Frontend/Xaberue.Playground.HospitalManager.AppointmentsPanel/src/app/app.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, MatCardModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppointmentsPanel';
+
+  appointments = [
+    <AppointmentModel>{ code: '1234', box: 'Box 1', state: 'Scheduled' },
+    <AppointmentModel>{ code: '1235', box: 'Box 2', state: 'Scheduled' },
+    <AppointmentModel>{ code: '1235', box: '-', state: 'Waiting' }
+  ]
+
+
+}
+
+
+interface AppointmentModel {
+  code: string;
+  box: string;
+  state: string;
 }
